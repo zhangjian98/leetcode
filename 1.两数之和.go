24cd9dc1dd4614b3746fa54df3eb08ad2af7f6ff
张建 <946350302@qq.com>
2019-09-10 17:45:25 +0800

    map

--- 1.两数之和.go
+++ 1.两数之和.go
@@ -4,6 +4,12 @@
  * [1] 两数之和
  */
 func twoSum(nums []int, target int) []int {
-    
+	index := make(map[int]int, len(nums))
+	for i, b := range nums {
+		if j, ok := index[target-b]; ok {
+			return []int{j, i}
+		}
+		index[b] = i
+	}
+	return nil
 }
-
